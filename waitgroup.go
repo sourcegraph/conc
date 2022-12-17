2e891e0f183da04f9a17f384924ce1a265d24f51
Camden Cheek <camden@ccheek.com>
2022-12-16 18:41:50 -0700

    add stacktrace to caught panic

--- waitgroup.go
+++ waitgroup.go
@@ -1,15 +1,24 @@
 package conc
 
 import (
+	"runtime/debug"
 	"sync"
 	"sync/atomic"
+
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// WaitGroup is the primary building block for scoped concurrency.
+// Goroutines can be spawned in the WaitGroup with the Go method,
+// and calling Wait() will ensure that each of those goroutines exits
+// before continuing. Any panics in a child goroutine will be caught
+// and propagated to the caller of Wait().
 type WaitGroup struct {
 	wg          sync.WaitGroup
-	caughtPanic atomic.Pointer[any]
+	caughtPanic atomic.Pointer[error]
 }
 
+// Go spawns a new goroutine in the WaitGroup
 func (h *WaitGroup) Go(f func()) {
 	h.wg.Add(1)
 	go func() {
@@ -18,6 +27,8 @@ func (h *WaitGroup) Go(f func()) {
 	}()
 }
 
+// Wait will block until all goroutines spawned with Go exit and will
+// propagate any panics spawned in a child goroutine.
 func (h *WaitGroup) Wait() {
 	h.wg.Wait()
 
@@ -30,7 +41,12 @@ func (h *WaitGroup) Wait() {
 // done should be called in a defer statement in a child goroutine
 func (h *WaitGroup) done() {
 	if r := recover(); r != nil {
-		h.caughtPanic.Store(&r)
+		err := errors.Newf(
+			"recovered from panic in child goroutine: %#v\n\nchild stacktrace:\n%s\n",
+			r,
+			debug.Stack(),
+		)
+		h.caughtPanic.Store(&err)
 	}
 	h.wg.Done()
 }
--- waitgroup_test.go
+++ waitgroup_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestWaitgroup(t *testing.T) {
+func TestWaitGroup(t *testing.T) {
 	t.Parallel()
 
 	t.Run("all spawned run", func(t *testing.T) {
