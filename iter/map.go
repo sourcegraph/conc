369d88e4cdd2b864e648291cf19d06f729ad4266
Robert Lin <robert@bobheadxi.dev>
2023-01-15 21:31:09 -0800

    iter: introduce Mapper type with configurable Iterator

--- iter/map.go
+++ iter/map.go
@@ -6,10 +6,34 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// Mapper can be used to configure the behaviour of iterators that
+// have a result type R, such as Map and MapErr. The zero value is
+// safe to use with reasonable defaults.
+//
+// Mapper is also safe for reuse and concurrent use.
+type Mapper[T, R any] struct {
+	// Iterator is the underlying implementation of Mapper methods.
+	// It can be used to configure the maximum number of goroutines
+	// that Mapper methods can use.
+	Iterator[T]
+}
+
 // Map applies f to each element of input, returning the mapped result.
+//
+// Map uses Iterator to perform the iteration, which always uses at most
+// runtime.GOMAXPROCS goroutines. For a configurable goroutine limit,
+// use a custom Mapper.
 func Map[T, R any](input []T, f func(*T) R) []R {
+	return Mapper[T, R]{}.Map(input, f)
+}
+
+// Map applies f to each element of input, returning the mapped result.
+//
+// Map uses Iterator to perform the iteration, using up to the configured
+// Iterator's maximum number of goroutines.
+func (m Mapper[T, R]) Map(input []T, f func(*T) R) []R {
 	res := make([]R, len(input))
-	ForEachIdx(input, func(i int, t *T) {
+	m.Iterator.ForEachIdx(input, func(i int, t *T) {
 		res[i] = f(t)
 	})
 	return res
@@ -17,13 +41,26 @@ func Map[T, R any](input []T, f func(*T) R) []R {
 
 // MapErr applies f to each element of the input, returning the mapped result
 // and a combined error of all returned errors.
+//
+// MapErr uses Iterator to perform the iteration, which always uses at
+// most runtime.GOMAXPROCS goroutines. For a configurable goroutine limit,
+// use a custom Mapper.
 func MapErr[T, R any](input []T, f func(*T) (R, error)) ([]R, error) {
+	return Mapper[T, R]{}.MapErr(input, f)
+}
+
+// MapErr applies f to each element of the input, returning the mapped result
+// and a combined error of all returned errors.
+//
+// MapErr uses Iterator to perform the iteration, using up to the configured
+// Iterator's maximum number of goroutines.
+func (m Mapper[T, R]) MapErr(input []T, f func(*T) (R, error)) ([]R, error) {
 	var (
 		res    = make([]R, len(input))
 		errMux sync.Mutex
 		errs   error
 	)
-	ForEachIdx(input, func(i int, t *T) {
+	m.Iterator.ForEachIdx(input, func(i int, t *T) {
 		var err error
 		res[i], err = f(t)
 		if err != nil {
